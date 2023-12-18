15549698a92b34c68f7e9c48c6fbd8fcb882d63f
TheStrechh <thestrechh@gmail.com>
2023-12-18 07:10:23 +0530

    taoyao: Add missing packages for camera

--- device.mk
+++ device.mk
@@ -116,7 +116,12 @@ PRODUCT_PACKAGES += \
     android.hidl.memory.block@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    android.hardware.graphics.common-V4-ndk
+    android.hardware.graphics.common-V4-ndk \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libcamera2ndk_vendor \
+    libion.vendor \
+    libgui_vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt \
@@ -236,6 +241,8 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhwbinder \
     android.hidl.manager@1.0.vendor
