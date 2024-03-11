6e1a066b7699e602bef8f47f789367a29c59baac
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-11 17:27:24 +0100

    sm8350-common: powerhint: Import powerhint.json from lahaina
    from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/blob/fourteen-qpr1/configs/powerhint_lahaina.json
    
    Change-Id: I2ddb226cfd379b6b287a4393058c12dd579d38de

--- common.mk
+++ common.mk
@@ -340,7 +340,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
