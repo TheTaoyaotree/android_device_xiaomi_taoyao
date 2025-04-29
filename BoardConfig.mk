67d5bc357e03dda77a228d43e2a1b0ed9a59abc7
Michael Bestas <mkbestas@gmail.com>
2025-04-29 11:41:39 +0200

    sm8350-common: Move Lineage PowerShare HAL to select()
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,9 +175,6 @@ TARGET_BOARD_PLATFORM := lahaina
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-xiaomi
 
-# PowerShare
-TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
