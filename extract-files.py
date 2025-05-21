5ba460e2e7082caa28b6f4504a9037b9fed36ff1
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-05-21 15:14:10 -0300

    taoyao: Updating camera blobs and sepolicies

--- extract-files.py
+++ extract-files.py
@@ -28,7 +28,6 @@ namespace_imports = [
     'vendor/qcom/opensource/display',
 ]
 
-
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
@@ -90,7 +89,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('xml=version', 'xml version'),
     'system/lib64/libmicampostproc_client.so': blob_fixup()
         .remove_needed('libhidltransport.so'),
-    ('system/lib64/libcamera_algoup_jni.xiaomi.so', 'system/lib64/libcamera_mianode_jni.xiaomi.so'): blob_fixup()
+    'system/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
+        .add_needed('libcamera_shim.so')
+        .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
+    'system/lib64/libcamera_mianode_jni.xiaomi.so': blob_fixup()
         .add_needed('libcamera_shim.so'),
 }  # fmt: skip
 
