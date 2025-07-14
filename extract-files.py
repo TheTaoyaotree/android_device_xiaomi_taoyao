7a6a26a29a06db2c97ca0869e099c0a366520188
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-07-14 12:11:53 -0300

    taoyao: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -96,6 +96,8 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
     'system/lib64/libcamera_mianode_jni.xiaomi.so': blob_fixup()
         .add_needed('libcamera_shim.so'),
+    ('vendor/lib/hw/audio.primary.lahaina.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
