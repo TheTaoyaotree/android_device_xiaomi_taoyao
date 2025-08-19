3fa52d53313566f5a3b50544ffd185a39cc19d77
Joey Huab <jhuab@edukasyon.ph>
2025-08-19 12:09:53 +0900

    taoyao: Revert "[SQUASH] sm8350-common: fastcharge implementation"
    This reverts commit d2793a3f48ad0a5b92c3b86c5c8f9b2c14779ccc.

--- device.mk
+++ device.mk
@@ -172,10 +172,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.venus
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
