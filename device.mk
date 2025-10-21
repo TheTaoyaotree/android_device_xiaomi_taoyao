589477aa2523dd92424056b20c53e1612a59f7ac
yaseakun <agathagemink2014@gmail.com>
2025-10-21 16:08:23 +0000

    taoyao: Update blobs from V816.0.27.0.ULIMIXM

--- device.mk
+++ device.mk
@@ -464,10 +464,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-# TimeKeep
-PRODUCT_PACKAGES += \
-    TimeKeep
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
